16bfbb5ece96f702e9270b96ac7b9831973968d4
Oliver Rhyme Guibone Añasco <35908542+Bberx@users.noreply.github.com>
2019-12-06 20:59:21 +0800

    Update sources Migrate to AndroidX

--- app/src/main/java/com/supercilex/poiandroid/demo/MainActivity.kt
+++ app/src/main/java/com/supercilex/poiandroid/demo/MainActivity.kt
@@ -4,8 +4,8 @@ import android.Manifest
 import android.content.Intent
 import android.os.Bundle
 import android.os.Environment
-import android.support.v4.content.FileProvider
-import android.support.v7.app.AppCompatActivity
+import androidx.core.content.FileProvider
+import androidx.appcompat.app.AppCompatActivity
 import android.util.Log
 import android.view.View
 import android.widget.Toast
